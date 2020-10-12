295df74b02989a2d45173a44dd3442c2afe7f8c7
Krishna <bkesari@outlook.com>
2020-10-12 14:56:24 -0400

    Added Bounds

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import data from './cities'
 
 class App extends Component {
   render(){
+      var centerLat = (data.minLat + data.maxLat) /2;
+      var distanceLat = data.maxLat - data.minLat;
+      var bufferLat = distanceLat * 0.05;
+      var centerLong = (data.minLong + data.maxLong) /2;
+      var distanceLong = data.maxLong - data.minLong;
+      var bufferLong = distanceLong * 0.15;
     return (
         <div>
             <h3 style={{textAlign: "center"}}>Most Populous cities in Asia</h3>
           <Map
             style={{height: "480px", width: "100%"}}
             zoom={1}
-            center={[-0.09, 51.505]}>
+            center={[centerLat, centerLong]}
+            bounds={[
+                [data.minLat - bufferLat, data.minLong - bufferLong],
+                [data.maxLat + bufferLat, data.maxLong + bufferLong]
+            ]}
+          >
               <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
               {data.city.map((city) => {
