2867d11f9c74758d548495c81b0f40978090b149
Krishna <bkesari@outlook.com>
2020-10-12 14:43:33 -0400

    Adding Coordinates data  and Title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
 import {Map, CircleMarker, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css"
+import data from './cities'
 
 class App extends Component {
   render(){
     return (
         <div>
+            <h3 style={{textAlign: "center"}}>Most Populous cities in Asia</h3>
           <Map
             style={{height: "480px", width: "100%"}}
             zoom={1}
             center={[-0.09, 51.505]}>
               <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              <CircleMarker center={[51.505, -0.09]}/>
+
+              {data.city.map((city) => {
+                  return(
+                     <CircleMarker
+                     center={[city["coordinates"][1], city["coordinates"][0]]}
+                     />
+                  )
+              })}
           </Map>
         </div>
     );
